aef62ed0d78942c1bc121e30f412668ce6f69ca2
rama-contoh <145236174+rama-contoh@users.noreply.github.com>
2025-06-20 22:38:01 +0700

    Update LevelProgressManajer.cs

--- Game/scripts/LevelProgressManajer.cs
+++ Game/scripts/LevelProgressManajer.cs
@@ -1,1 +1,29 @@
-{\rtf1}
+using UnityEngine;
+
+public class LevelProgressManager : MonoBehaviour
+{
+    // Menyimpan index level terakhir yang diselesaikan
+    public void SimpanProgressLevel(int levelIndex)
+    {
+        PlayerPrefs.SetInt("LevelSelesai", levelIndex);
+        PlayerPrefs.Save();
+        Debug.Log("Progress level disimpan: Level " + levelIndex);
+    }
+
+    // Mengambil index level terakhir yang disimpan
+    public int AmbilProgressLevel()
+    {
+        int level = PlayerPrefs.GetInt("LevelSelesai", 0); // default level 0
+        Debug.Log("Progress level terakhir: Level " + level);
+        return level;
+    }
+
+    // Contoh penggunaan saat start game
+    void Start()
+    {
+        int levelTerakhir = AmbilProgressLevel();
+
+        // Misalnya langsung lanjut ke level terakhir (opsional)
+        // UnityEngine.SceneManagement.SceneManager.LoadScene(levelTerakhir);
+    }
+}
